3791ab4229cab524dfcda8a5bda7f9e62a454f5d
Yasmin <alunoyasminsilva07@gmail.com>
2023-11-07 18:22:10 -0300

    autores e editoras

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const livroRoutes = require('./routes/livroRoutes');
+const autoresRoutes = require('./routes/autoresRoutes');
 
 const app = express();
 const PORT = 3000;
@@ -9,7 +10,9 @@ const PORT = 3000;
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/api/livros', livroRoutes);
+// Suas rotas
+app.use('/api/livros', livroRoutes); // Livros
+app.use('/api/autores', autoresRoutes); // Autores
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
